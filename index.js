2ca86afc3e980473d051b8f7bbcf6c963846fe45
al66 <andreas.leinen@imicros.de>
2020-06-28 15:04:12 +0200

    corrected inex

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 /**
- * @license MIT, imicros.de (c) 2019 Andreas Leinen
+ * @license MIT, imicros.de (c) 2020 Andreas Leinen
  */
 "use strict";
 
 module.exports = {
-    // Static [kafka]
-    Publisher: require("./lib/publisher"),
-    Subscriber: require("./lib/subscriber"),
-    Broker: require("./lib/broker"),
-    Consumer: require("./lib/consumer"),
-    Admin: require("./lib/admin")
+    Transporter: require("./lib/transporter")
 };
